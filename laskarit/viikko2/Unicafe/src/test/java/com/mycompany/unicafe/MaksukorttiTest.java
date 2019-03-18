21b8915c64074799997b8271319e78a08f24d936
Riina-Alisa <riina.alisa@live.com>
2019-03-18 22:50:14 +0200

    Unicafe tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,35 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    @Test
+    public void kortinSaldoOikeinAlussa() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void rahanLisaaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(200);
+        assertEquals("saldo: 2.10", kortti.toString());
+    }
+
+    @Test
+    public void saldoVaheneeOikeinKunRahaaTarpeeksi() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(20);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void palauttaaTrueJosRahatRiittävät() {
+        boolean isTrue = kortti.otaRahaa(10);
+        assertTrue(isTrue);
+        boolean isFalse = kortti.otaRahaa(20);
+        assertTrue(!isFalse);
+    }
 }
